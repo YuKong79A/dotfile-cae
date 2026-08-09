14a07d311dfa7ecaf7a8527baa68dd32d3d13e86
YuKong79A <you@example.com>
2026-08-09 17:49:33 +0800

    add unified transparency toggle

--- .config/caelestia/hypr-vars.lua
+++ .config/caelestia/hypr-vars.lua
@@ -1,21 +1,16 @@
 return {
     -- Apps
     browser                    = "helium-browser",
+    fileExplorer               = thunar,
 
     -- Blur
     blurEnabled                = true,
     blurSpecialWs              = true,
     blurPopups                 = true,
     blurInputMethods           = true,
-    blurSize                   = 8,
-    blurPasses                 = 2,
-    blurXray                   = false,
-
-    -- Shadow
-    shadowEnabled              = false,
 
     -- Window styling
-    windowOpacity              = 0.8,
+    windowOpacity              = 1.0,
 
     -- Misc
     cursorTheme                = "Bibata-Modern-Classic",
